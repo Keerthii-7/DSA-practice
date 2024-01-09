c3ae9ea648e7a3aeb1221d424fdf23d31cc1b6d3
Keerthii-7 <102132720+Keerthii-7@users.noreply.github.com>
2024-01-09 22:10:43 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (9.90%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        ArrayList<Integer> l1=new ArrayList<>();
+        ArrayList<Integer> l2=new ArrayList<>();
+        helperFun(root1,l1);
+        helperFun(root2,l2);
+        if(l1.size()!=l2.size()){
+            return false;
+        }
+        for(int i=0;i<l1.size();i++)
+        {
+            if(l1.get(i)!=l2.get(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public void helperFun(TreeNode node,ArrayList<Integer> sol){
+        if(node==null){
+            return;
+        }
+        if(node.left==null && node.right==null){
+            sol.add(node.val);
+        }
+        helperFun(node.left,sol);
+        helperFun(node.right,sol);
+    }
+}
