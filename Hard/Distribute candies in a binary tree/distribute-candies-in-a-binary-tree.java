bd90d69929fccbe24fd494feabd4099c3113b504
Keerthii-7 <102132720+Keerthii-7@users.noreply.github.com>
2024-01-20 10:44:27 +0530

    Updated Solution - GfG to GitHub

--- Hard/Distribute candies in a binary tree/distribute-candies-in-a-binary-tree.java
+++ Hard/Distribute candies in a binary tree/distribute-candies-in-a-binary-tree.java
@@ -107,19 +107,18 @@ class Node {
 
 
 class Solution
-{
-    public static int distributeCandy(Node root)
+{   
+    int steps=0;
+    public int distributeCandy(Node root)
     {
-        return traverse(root)[2];
+        helperFun(root);
+        return steps;
     }
-    
-    public static int[] traverse(Node node) {
-        if (node == null) return new int[]{0, 0, 0};
-        int[] left = traverse(node.left);
-        int[] right = traverse(node.right);
-        int nodes = left[0] + right[0] + 1;
-        int candies = left[1] + right[1] + node.data;
-        int moves = left[2] + right[2] + Math.abs(left[1]-left[0]) + Math.abs(right[1]-right[0]);
-        return new int[]{nodes, candies, moves};
+    public int helperFun(Node root){
+        if(root==null) return 0;
+        int l=helperFun(root.left);
+        int r=helperFun(root.right);
+        steps+=Math.abs(l)+Math.abs(r);
+        return (l+r+root.data-1);
     }
 }
