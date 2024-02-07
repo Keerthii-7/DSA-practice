8f1cab816f820406b0ec73cbfc79039d58d73850
Keerthii-7 <102132720+Keerthii-7@users.noreply.github.com>
2024-02-07 09:12:15 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (41.13%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -19,20 +19,4 @@ class Solution {
         k--;
     }
 }
-public static void main(String args[]){
-    Scanner in=new Scanner(System.in);
-    int nums1[]=new int[400];
-    int nums2[]=new int[200];
-    for(int i=0;i<nums1.length;i++){
-        nums1[i]=in.nextInt();
-    }
-    int m=in.nextInt();
-    for(int i=0;i<nums2.length;i++){
-        nums2[i]=in.nextInt();
-    }
-    int n=in.nextInt();
-    merge(nums1,m,nums2,n);
-    System.out.println(nums1);
-}
-
 }
