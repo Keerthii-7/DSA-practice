53b58a17e961c30faf0d300e632b39e6fde0bd9e
Keerthii-7 <102132720+Keerthii-7@users.noreply.github.com>
2024-08-15 10:30:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GfG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data);
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+            Solution obj = new Solution();
+            head = obj.addOne(head);
+            printList(head);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node addOne(Node head) {
+        // code here.
+        head=reverseList(head);
+        Node curr=head;
+        int sum=curr.data+1;
+        int carry=0;
+        Node prev=null;
+        while(curr!=null){
+            carry=sum/10;
+            sum=sum%10;
+            curr.data=sum;
+            prev=curr;
+            curr=curr.next;
+            if(curr!=null){
+                sum=carry+curr.data;
+            }
+        }
+        if(carry==1){
+            prev.next=new Node(1);
+        }
+        head=reverseList(head);
+        return head;
+    }
+    public static Node reverseList(Node head){
+        Node curr=head;
+        Node prev=null;
+        while(curr!=null){
+            Node next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
