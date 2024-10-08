ed01fa9991b3ea98984ec2364037ef8ff1bd97c7
Keerthii-7 <102132720+Keerthii-7@users.noreply.github.com>
2024-10-08 11:59:28 +0530

    Time: 14 ms (76.82%), Space: 55.7 MB (23.18%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,7 +1,6 @@
 class Solution {
     public int minSwaps(String s) {
         int bal=0;
-        int swaps=0;
         for(char ch:s.toCharArray()){
             if(ch=='['){
                 bal++;
